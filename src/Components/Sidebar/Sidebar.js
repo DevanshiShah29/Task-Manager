e6fd58a31a48fcb98a13069e31643ebdc5752746
Devanshi <60215921+DevanshiShah29@users.noreply.github.com>
2020-10-16 14:24:09 +0530

    Add files via upload
    Initial commit, Dependencies, sass and redux setup added

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { IconContext } from 'react-icons';
+import { SidebarData } from './SidebarData';
+
+function Sidebar() {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar)
+    return (
+        <>
+        <IconContext.Provider value={{ color: "#f5f5f5", className: "global-class-name" }}>
+            <div className="navbar">
+                <Link to="#" className='menu-bar'>
+                    <FaIcons.FaBars onClick={showSidebar}/>
+                </Link>
+            </div>
+            <nav className={ sidebar ? 'nav-menu active' : 'nav-menu' }>
+                <ul className="nav-menu-items" onClick={showSidebar}>
+                    <li className="navbar-toggle">
+                        <Link to="#" className="menu-bar"><AiIcons.AiOutlineClose/></Link>
+                    </li>
+                    { SidebarData.map((item, index) => {
+                        return (
+                          <li key={index} className={item.cName}>
+                            <Link to={item.path}>
+                              {item.icon}
+                              <span>{item.title}</span>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                </ul>
+            </nav>
+            </IconContext.Provider>
+        </>
+    )
+}
+
+export default Sidebar
